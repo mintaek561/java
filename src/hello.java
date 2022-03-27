bfbe311b7f186887c111825534e834ada65e80f6
mintaek <dhalsxor561@naver.com>
2022-03-27 12:16:46 +0900

    3주차 JAVA

--- src/hello.java
+++ src/hello.java
@@ -4,6 +4,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java!!");
+		System.out.println("I'm a new Java programmer");
 	}
 
 }
