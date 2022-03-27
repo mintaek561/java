71fd3ca960655f0c1c2c8cd978b5a21bf5ab36af
mintaek <dhalsxor561@naver.com>
2022-03-27 13:24:18 +0900

    while 문과 do while 문을 사용해봤다.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
 	public static void main(String[] args){
-		System.out.println("Hello Java!!");
+		int i = 0;
+		while(i < 5) {
+			System.out.println("환영합니다. ");
+			i++
+		}
+		System.out.println("반복이 종료되었습니다.");
 	}
 }
